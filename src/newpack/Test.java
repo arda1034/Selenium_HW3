d69be0a5d9948f4221b219afae0a3666d1b00039
arda1034 <h.arda1034@gmail.com>
2021-11-16 00:22:25 +0300

    Selenium_HW4

--- src/newpack/Test.java
+++ src/newpack/Test.java
@@ -1,5 +1,7 @@
 package newpack;
+
 import java.time.Duration;
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -11,10 +13,19 @@ public class Test {
         WebDriver driver=new ChromeDriver(); //Initiating your chromedriver
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); //Applied wait time
         driver.manage().window().maximize(); //maximize window
-        driver.get("http://localhost:8080/demo10_war_exploded/"); //open browser with desried URL
-        WebElement a = driver.findElement(By.id("10"));
-        String b = a.getText();
+        driver.get("https://www.n11.com/"); //open browser with desired URL
+        WebElement input = driver.findElement(By.id("searchData"));
+        input.sendKeys("Bilgisayar");
+        //Thread.sleep(4000);
+        WebElement search = driver.findElement(By.className("searchBtn"));
+        search.click();
+        //Thread.sleep(4000);
+        WebElement search_result = driver.findElement(By.className("resultText"));
+        boolean res = search_result.getText().contains("Bilgisayar");
+        //Thread.sleep(4000);
+        String result = "Arama Başarısız.";
+        if(res) result = "Arama Başarılı.";
+        System.out.println(result);
         driver.close(); //closing the browser
-        System.out.println(b);
     }
 }
